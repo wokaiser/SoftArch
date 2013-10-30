36595cb46739a37fe4a5f349d7b4c1b38d3a49f3
wokaiser <Wolfgang.Kaiser3012@gmx.de>
2013-10-30 19:59:25 +0100

    Missed to add one package.

---
+++ SE.Battleship/src/database/Db4oDatabase.java
@@ -0,0 +1,34 @@
+package database;
+
+import java.util.List;
+
+import model.general.GameContent;
+import interfaces.IDatabase;
+
+public class Db4oDatabase implements IDatabase {
+
+	@Override
+	public GameContent load(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(String name, GameContent content) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

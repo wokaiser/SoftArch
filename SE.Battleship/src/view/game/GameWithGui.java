18077822256cde8abb362b94432615dea64cf79a
wokaiser <Wolfgang.Kaiser3012@gmx.de>
2013-11-02 14:29:58 +0100

    Moved error message creation from GUI to controller

--- SE.Battleship/src/view/game/GameWithGui.java
+++ SE.Battleship/src/view/game/GameWithGui.java
@@ -151,7 +151,8 @@ public class GameWithGui implements IObserver {
 						break;
 					}
 					if (!controller.saveGame(name)) {
-						JOptionPane.showMessageDialog(mainFrame, "Savegame already taken, choose another one!", "", JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(mainFrame, controller.getStatus().getError(), "", JOptionPane.WARNING_MESSAGE);
+						controller.getStatus().clearError();
 						continue;
 					}
 					break;
